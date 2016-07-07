43b6b8710415537ffd294f0642f3ec6236ee3327
Nils Butenschön <butenschoen@bdrops.de>
2016-07-07 16:39:37 +0200

    Add FUTURE comment.

--- es6/decofill.js
+++ es6/decofill.js
@@ -8,6 +8,7 @@ const forEach = [].forEach,
         return CSS.supports('( box-decoration-break: clone ) or ( -webkit-box-decoration-break: clone )');
     },
     // TODO: Detect the wrapping element automatically and insert the slices correspondingly
+    // FUTURE: Set hyphens (if any) to "none" and detect line wrapping by inserting <span>'s where appropriate
     decoFill = els => {
         if (!hasBoxDecorationBreak()) {
             forEach.call(els, textEl => {
